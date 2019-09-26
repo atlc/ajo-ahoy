e49b0cd1203fbc0ce62f03a0c39ca5429fadfeef
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-09-25 22:31:16 -0500

    Update background.js

--- background.js
+++ background.js
@@ -11,10 +11,12 @@ function walkThroughDOM() {
 function garlicTime(childTextNode) {
 	let v = childTextNode.nodeValue;
 
-    v = v.replace(/cloves garlic/gi, 'bulbs of garlic');
-	v = v.replace(/cloves of garlic/gi, 'bulbs of garlic');
-    v = v.replace(/clove of garlic/gi, 'bulb of garlic');
+	v = v.replace(/bulbs of garlic/gi, 'dozen heads of garlic');
+	v = v.replace(/bulb of garlic/gi, 'dozen heads of garlic');
 	v = v.replace(/garlic clove/gi, 'garlic bulb');
+      	v = v.replace(/cloves garlic/gi, 'bulbs of garlic');
+	v = v.replace(/cloves of garlic/gi, 'bulbs of garlic');
+    	v = v.replace(/clove of garlic/gi, 'bulb of garlic');
 
 	childTextNode.nodeValue = v;
 }
